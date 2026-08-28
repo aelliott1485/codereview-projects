af2f5262a5bb40bc6fc74decff9f1ad9b5acb243
Andrew Elliott <aelliott1485@users.noreply.github.com>
2026-08-28 15:50:38 -0700

    for creating encryption signatures: simplify code, update per PSR-12, add error checking

--- openssl-rsa-signature-creation/createSignature.php
+++ openssl-rsa-signature-creation/createSignature.php
@@ -1,4 +1,4 @@
-<?
+<?php
 // Source - https://codereview.stackexchange.com/q/212989
 // Posted by Ama, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-08-28, License - CC BY-SA 4.0
@@ -6,20 +6,26 @@
 header('Content-Type: text/plain; charset=utf-8');
 
 // Verify connection is secure
-if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS']=="off")
-{
-    echo "Unauthorized Access";
-    exit;
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
+    exit('Unauthorized Access');
 }
 
 // Data to Sign
-$data = base64_decode(file_get_contents('php://input'));
+$dataFromInput = file_get_contents('php://input');
+if (empty($dataFromInput)) {
+    exit('Unable to read data from input');
+}
+$data = base64_decode($dataFromInput);
 
 //Load Private Key
-$private_key_pem = openssl_pkey_get_private('file:///path/protected/by/dotHtaccess/private_key.pem', "my passphrase to protect my private key; add random characters like $, ?, #, & or ! for improved security");
+$private_key_pem = openssl_pkey_get_private(
+    'file:///path/protected/by/dotHtaccess/private_key.pem',
+    'my passphrase to protect my private key; add random characters like $, ?, #, & or ! for improved security'
+);
 
 //Create Signature
-openssl_sign($data, $signature, $private_key_pem, OPENSSL_ALGO_SHA256);
+$signatureGenerated = openssl_sign($data, $signature, $private_key_pem, OPENSSL_ALGO_SHA256);
+if (!$signatureGenerated) {
+   exit('Unable to generate signature');
+}
 echo base64_encode($signature);
-
-?>
