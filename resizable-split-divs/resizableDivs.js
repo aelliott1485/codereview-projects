bc4ca2f2d1cc2fc610e5f4d913cbb482f1ea5d59
Andrew Elliott <aelliott1485@users.noreply.github.com>
2026-09-07 10:44:20 -0700

    Update code per my review in https://codereview.stackexchange.com/a/232396/120114

--- resizable-split-divs/resizableDivs.js
+++ resizable-split-divs/resizableDivs.js
@@ -2,108 +2,116 @@
 // Posted by Muhammad Azizi Abdul Aziz, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-09-05, License - CC BY-SA 4.0
 
-var dividerRef = '';
-var currentDivider = null;
-
-var leftDivider = null;
-var rightDivider = null;
-var leftRightDivider = null;
-
-var topLeft = null;
-var topRight = null;
-var bottomLeft = null;
-var bottomRight = null;
 document.addEventListener('DOMContentLoaded', function () {
     var app = new Vue({
         el: '#app',
-        methods:
-            {
-                dividerDragStart: function(e) {
-                    e.dataTransfer.setDragImage(new Image, 0, 0);
-                },
-                dividerDrag: function(e) {
-                    if (dividerRef == 'lrDivider') {
-                        currentDivider.style.left = e.clientX + 'px';
-
-                        leftDivider.style.width = (e.clientX + 2) + 'px';
-
-                        rightDivider.style.left = (e.clientX) + 'px';
-                        rightDivider.style.width = (window.innerWidth - e.clientX + 2) + 'px';
-
-                        topLeft.style.width = e.clientX + 'px';
-                        bottomLeft.style.width = e.clientX + 'px';
-
-                        topRight.style.left = e.clientX + 'px';
-                        topRight.style.width = (window.innerWidth - e.clientX + 2) + 'px';
-                        bottomRight.style.left = e.clientX + 'px';
-                        bottomRight.style.width = (window.innerWidth - e.clientX + 2) + 'px';
-                    } else if (dividerRef == 'rtbDivider') {
-                        currentDivider.style.top = (e.clientY) + 'px';
-
-                        topRight.style.height = (e.clientY) + 'px'
-
-                        bottomRight.style.height = (window.innerHeight - e.clientY) + 'px';
-                        bottomRight.style.top = (e.clientY) + 'px';
-                    } else if (dividerRef == 'ltbDivider') {
-                        currentDivider.style.top = (e.clientY) + 'px';
-
-                        topLeft.style.height = (e.clientY) + 'px'
-
-                        bottomLeft.style.height = (window.innerHeight - e.clientY) + 'px';
-                        bottomLeft.style.top = (e.clientY) + 'px';
-                    }
-                },
-                dividerMouseDown: function(name) {
-                    dividerRef = name;
-                    currentDivider = this.$refs[dividerRef];
-                },
-                dividerDragEnd: function(e) {
-                    if (dividerRef == 'lrDivider') {
-                        currentDivider.style.left = e.clientX + 'px';
-                        leftDivider.style.width = (e.clientX + 2) + 'px';
-
-                        rightDivider.style.left = (e.clientX) + 'px';
-                        rightDivider.style.width = (window.innerWidth - e.clientX + 2) + 'px';
-
-                        topLeft.style.width = e.clientX + 'px';
-                        bottomLeft.style.width = e.clientX + 'px';
-
-                        topRight.style.left = e.clientX + 'px';
-                        topRight.style.width = (window.innerWidth - e.clientX + 2) + 'px';
-                        bottomRight.style.left = e.clientX + 'px';
-                        bottomRight.style.width = (window.innerWidth - e.clientX + 2) + 'px';
-                    } else if (dividerRef == 'rtbDivider') {
-                        currentDivider.style.top = (e.clientY) + 'px';
-
-                        topRight.style.height = (e.clientY) + 'px'
-
-                        bottomRight.style.height = (window.innerHeight - e.clientY) + 'px';
-                        bottomRight.style.top = (e.clientY) + 'px';
-                    } else if (dividerRef == 'ltbDivider') {
-                        currentDivider.style.top = (e.clientY) + 'px';
-
-                        topLeft.style.height = (e.clientY) + 'px'
-
-                        bottomLeft.style.height = (window.innerHeight - e.clientY) + 'px';
-                        bottomLeft.style.top = (e.clientY) + 'px';
-                    }
-
-                    dividerRef = '';
-                    currentDivider = null;
+        data: {
+            lrDividerPos: '',
+            rtbDividerPos: '',
+            ltbDividerPos: '',
+        },
+        computed: {
+            bottomLeftStyle: function() {
+                const style = {};
+                if (this.lrDividerPos) {
+                    style.width = this.lrDividerPos + 'px';
+                }
+                if (this.ltbDividerPos) {
+                    style.height = (window.innerHeight - this.ltbDividerPos) + 'px';
+                    style.top = this.ltbDividerPos + 'px';
+                }
+                return style;
+            },
+            bottomRightStyle: function() {
+                const style = {};
+                if (this.lrDividerPos) {
+                    style.left = this.lrDividerPos + 'px';
+                    style.width = (window.innerWidth - this.lrDividerPos + 2) + 'px';
+                }
+                if (this.rtbDividerPos) {
+                    style.top = this.rtbDividerPos + 'px';
+                    style.height = (window.innerHeight - this.rtbDividerPos) + 'px';
+                }
+                return style;
+            },
+            leftDividerStyles: function() {
+                if (this.lrDividerPos) {
+                    return {
+                        width: (this.lrDividerPos + 2) + 'px'
+                    };
+                }
+                return {};
+            },
+            ltbDividerStyles: function() {
+                const style = {};
+                if (this.lrDividerPos) {
+                    style.width = this.lrDividerPos + 2 + 'px';
+                }
+                if (this.ltbDividerPos) {
+                    style.top = this.ltbDividerPos + 'px';
+                }
+                return style;
+            },
+            lrDividerStyles: function() {
+                if (this.lrDividerPos) {
+                    return {
+                        left: this.lrDividerPos + 'px'
+                    };
+                }
+                return {};
+            },
+            rtbDividerStyles: function() {
+                const style = {};
+                if (this.lrDividerPos) {
+                    style.left = this.lrDividerPos + 'px';
+                    style.width = (window.innerWidth - this.lrDividerPos + 2) + 'px';
+                }
+                if (this.rtbDividerPos) {
+                    style.top = this.rtbDividerPos + 'px';
+                }
+                return style;
+            },
+            topLeftStyle: function() {
+                const style = {};
+                if (this.ltbDividerPos) {
+                    style.height = this.ltbDividerPos + 'px';
+                }
+                if (this.lrDividerPos) {
+                    style.width = this.lrDividerPos + 'px';
+                }
+                return style;
+            },
+            topRightStyle: function() {
+                const style = {};
+                if (this.lrDividerPos) {
+                    style.left = this.lrDividerPos + 'px';
+                    style.width = (window.innerWidth - this.lrDividerPos + 2) + 'px';
+                }
+                if (this.rtbDividerPos) {
+                    style.height = this.rtbDividerPos + 'px';
+                }
+                return style;
+            }
+        },
+        methods: {
+            lrDividerDrag: function(e) {
+                if (e.clientX) {
+                    this.lrDividerPos = e.clientX;
                 }
             },
-        mounted() {
-            topLeft = this.$refs.topLeft;
-            topRight = this.$refs.topRight;
-            bottomLeft = this.$refs.bottomLeft;
-            bottomRight = this.$refs.bottomRight;
-
-            var heightHalf = (window.innerHeight - 80) / 2;
-            var widthHalf = window.innerWidth / 2;
-
-            leftDivider = this.$refs.ltbDivider;
-            rightDivider = this.$refs.rtbDivider;
-            leftRightDivider = this.$refs.lrDivider;
+            ltbDividerDrag: function(e) {
+                if (e.clientY) {
+                    this.ltbDividerPos = e.clientY;
+                }
+            },
+            rtbDividerDrag: function(e) {
+                if (e.clientY) {
+                    this.rtbDividerPos = e.clientY;
+                }
+            },
+            dividerDragStart: function(e) {
+                e.dataTransfer.setDragImage(new Image, 0, 0);
+            }
         }
-    })
-})
+    });
+});
